43deb441c4b1d6daed24aeb4a3416eb8b3e004ed
Dmalasky <mal19013@byui.edu>
2024-10-16 18:43:37 -0600

    asdf

---
+++ week-5/pieceKing.cpp
@@ -0,0 +1,9 @@
+#include "pieceKing.h"
+
+void King::getMoves(set<Move>& moves, const Board& board) const
+{
+}
+
+void King::display(ogstream* pgout) const
+{
+}
---
+++ week-5/pieceRook.cpp
@@ -0,0 +1,9 @@
+#include "pieceRook.h"
+
+void Rook::getMoves(set<Move>& moves, const Board& board) const
+{
+}
+
+void Rook::display(ogstream* pgout) const
+{
+}
