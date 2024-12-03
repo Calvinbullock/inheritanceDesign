f081dbebcf9c1d2029962c02b3b0bd9a09a39a66
Calvinbullock <calvinnbullock@gmail.com>
2024-12-02 19:32:49 -0700

    created fragments class

---
+++ week-10/fragments.h
@@ -0,0 +1,32 @@
+
+/***********************************************************************
+ * Source File:
+ *    Fragment class
+ * Author:
+ *    Daniel Malasky & Calvin Bullock
+ * Summary:
+ *    Everything to know about Fragment's
+ ************************************************************************/
+
+#pragma once
+
+#include "entity.h"
+#include "acceleration.h"
+
+/***********************************************************************
+ * Fragment
+ *    Everything to know about Fragment's
+ ************************************************************************/
+class Fragment: public Entity
+{
+
+public:
+   // Constructors
+   Fragment() : Entity() {}
+   Fragment(Position& pos, Velocity& vel, Angle& a, bool isBroke = false)
+      : Entity(pos, vel, a, isBroke) {}
+
+   virtual void draw(ogstream& gout) { gout.drawFragment(position, angle.getRadians()); }
+
+protected:
+};
